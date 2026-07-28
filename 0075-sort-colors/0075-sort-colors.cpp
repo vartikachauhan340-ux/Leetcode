32df64555f789ed825c2e483d664c8c9101b748e
Vartika chauhan <vartikachauhan340@gmail.com>
2026-07-28 10:01:00 -0700

    Time: 0 ms (100%), Space: 11.7 MB (49.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int n= nums.size();
+        int low=0, mid=0, high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else {
+                swap(nums[high], nums[mid]);
+                high--;    
+            }
+        }
         
     }
 };
