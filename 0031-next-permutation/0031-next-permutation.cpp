b85ccea8498a15781361cb6577021918258ec159
Vartika chauhan <vartikachauhan340@gmail.com>
2026-07-30 10:31:46 -0700

    Time: 0 ms (100%), Space: 15.7 MB (45.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n= nums.size();
+        int pivot =-1;
+        for(int i= n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+        if(pivot == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i= n-1; i> pivot; i--){
+            if(nums[pivot] < nums[i]){
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        int i= pivot+1, j=n-1;
+        while(i<=j){
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
     }
 };
