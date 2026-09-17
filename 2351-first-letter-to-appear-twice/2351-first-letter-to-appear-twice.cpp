c2de2c0b7477586d5f2b5f56e955c09458c56252
Vartika chauhan <vartikachauhan340@gmail.com>
2026-09-17 23:02:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.31%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        int freq[26] = {0};
+        for(char ch : s){
+            freq[ch - 'a']++;
+            if(freq[ch - 'a']==2){
+                return ch;
+            }
+        }
+        return ' ';
+        
+
+    }
+};
