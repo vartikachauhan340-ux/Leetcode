23363befcef7a867168718fe8bd2800b4a9cbdda
Vartika chauhan <vartikachauhan340@gmail.com>
2026-09-19 00:02:12 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        long long low=1, high=x;
+        int ans=1;
+        while(low<=high){
+            long long mid= low+(high-low)/2;
+            if(mid*mid <=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
